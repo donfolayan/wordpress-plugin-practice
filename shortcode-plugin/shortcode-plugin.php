271b9dc835df53ca76aff10a246322f7fa0fcd30
Donald Folayan <donfolayan@gmail.com>
2025-06-03 16:09:18 +0100

    Add shortcodes for listing posts with database operations

--- shortcode-plugin/shortcode-plugin.php
+++ shortcode-plugin/shortcode-plugin.php
@@ -32,4 +32,51 @@ function sp_handle_student_data($attributes){
              Email- {$attr['email']}</p>";
 }
 
-// Shortcode with dynamic content
+// Shortcode with DB Operation
+
+function sp_list_posts(){
+
+    global $wpdb;
+
+    $table_name = $wpdb->prefix . "posts";
+    $posts = $wpdb->get_results("SELECT * FROM $table_name WHERE post_status = 'publish' && post_type = 'post'");
+    if (empty($posts)) {
+        return "<p>No posts found.</p>";
+    } else {
+        $outputHtml = "<ul>";
+
+        foreach ($posts as $post){
+            $outputHtml .= "<li>" . $post->post_title . "</li>";
+        }
+    }
+    $outputHtml .= "</ul>";
+    return $outputHtml;
+    
+}
+
+function sp_list_post_with_wp_posts($attributes){
+    $attr = shortcode_atts(array(
+        "number" => 5
+    ), $attributes, "list_posts");
+
+    $query = new WP_Query(array(
+        "post_type" => "post",
+        "posts_per_page" => $attr['number'],
+        "post_status" => "publish"
+    ));
+    
+    if ($query->have_posts()){
+
+        $outputHtml = "<ul>";
+        while ($query->have_posts()) {
+            $query->the_post();
+            $outputHtml .= "<li><a href='". get_the_permalink() . "'>" . get_the_title() ."</a></li>";
+        }
+        $outputHtml .= "</ul>";
+        return $outputHtml;
+    } else {
+        return "<p>No posts found.</p>";
+    }
+}
+
+add_shortcode("list_posts", "sp_list_post_with_wp_posts");
